5999e495946742dd2e789e5149176a8497b10f2d
Luis Scandelai <saeslhs@gmail.com>
2022-07-29 21:07:32 -0300

    Installing i18next for translation and changing index to support it

--- front-end/src/index.tsx
+++ front-end/src/index.tsx
@@ -1,7 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import i18n from "i18next";
+import { useTranslation, initReactI18next } from "react-i18next";
 import "./index.css";
-import App from "./App";
+
+i18n.use(initReactI18next).init({
+  resources: {
+    en: {
+      translation: {
+        "Welcome to React": "Welcome to React and react-i18next",
+      },
+    },
+    ptbr: {
+      translation: {
+        "Welcome to React": "Seja bem-vindo ao React e react-i18next",
+      },
+    },
+  },
+  lng: "en",
+  fallbackLng: "en",
+});
+
+function App() {
+  const { t } = useTranslation();
+
+  return <h2>{t("Welcome to React")}</h2>;
+}
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
