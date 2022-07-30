6ae109d8c55ad74440aa516d18cb21328a4ef40d
Luis Scandelai <saeslhs@gmail.com>
2022-07-29 23:53:02 -0300

    Adding multiple i18next dependencies, js-cookies and structured translation

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,8 +1,17 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 import "./App.css";
+import Temp from "./components/Temp";
 
 function App() {
-  return <div className="App"></div>;
+  const { t } = useTranslation();
+
+  return (
+    <div className="container">
+      <div className="d-flex flex-column align-items-start">{t("hello")}</div>
+      <Temp />
+    </div>
+  );
 }
 
 export default App;
--- front-end/src/index.tsx
+++ front-end/src/index.tsx
@@ -1,37 +1,49 @@
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import i18n from "i18next";
-import { useTranslation, initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import { initReactI18next } from "react-i18next";
+import HttpApi from "i18next-http-backend";
+import App from "./App";
 import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-i18n.use(initReactI18next).init({
-  resources: {
-    en: {
-      translation: {
-        "Welcome to React": "Welcome to React and react-i18next",
-      },
+i18n
+  .use(initReactI18next)
+  .use(LanguageDetector)
+  .use(HttpApi)
+  .init({
+    supportedLngs: ["en", "pt-BR"],
+    detection: {
+      order: [
+        "cookie",
+        "navigator",
+        "htmlTag",
+        "localStorage",
+        "path",
+        "subdomain",
+      ],
+      caches: ["cookie"],
     },
-    ptbr: {
-      translation: {
-        "Welcome to React": "Seja bem-vindo ao React e react-i18next",
-      },
+    fallbackLng: "en",
+    backend: {
+      loadPath: "/assets/locales/{{lng}}/translation.json",
     },
-  },
-  lng: "en",
-  fallbackLng: "en",
-});
+  });
 
-function App() {
-  const { t } = useTranslation();
-
-  return <h2>{t("Welcome to React")}</h2>;
-}
+const loadingMarkup = (
+  <div className="py-4 text-center">
+    <h2>Loading Website...</h2>
+  </div>
+);
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Suspense fallback={loadingMarkup}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Suspense>
 );
