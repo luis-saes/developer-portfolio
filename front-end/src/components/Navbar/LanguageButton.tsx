e7a9f4ab5c282685a4c84eadbaab7f2ebe9e8713
Luis Scandelai <saeslhs@gmail.com>
2022-07-30 15:17:22 -0300

    closes #2

---
+++ front-end/src/components/Navbar/LanguageButton.tsx
@@ -0,0 +1,50 @@
+import React, { useContext } from "react";
+import { ThemeContext } from "../../context/ThemeContext";
+import { useTranslation } from "react-i18next";
+import i18next from "i18next";
+import Cookies from "js-cookie";
+import styles from "./LanguageButton.module.scss";
+
+const LanguageButton = () => {
+  const { theme } = useContext(ThemeContext);
+  const { t } = useTranslation();
+
+  const languages = [
+    {
+      code: "en",
+      name: t("english"),
+    },
+    {
+      code: "pt-BR",
+      name: t("portuguese"),
+    },
+  ];
+
+  const currentLanguageCode = Cookies.get("i18next") || "en";
+
+  return (
+    <div className={styles.horFlex}>
+      <select
+        name="languages"
+        id="languages"
+        className={`${styles[theme]} ${styles.select}`}
+      >
+        {languages.map(({ code, name }) => {
+          return (
+            <option
+              key={code}
+              onClick={() => i18next.changeLanguage(code)}
+              disabled={code === currentLanguageCode}
+              selected={code === currentLanguageCode}
+            >
+              {name}
+            </option>
+          );
+        })}
+      </select>
+      <div className={`${styles[theme]} ${styles.verticalLine}`}></div>
+    </div>
+  );
+};
+
+export default LanguageButton;
