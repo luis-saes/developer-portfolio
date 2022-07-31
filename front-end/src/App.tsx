8c435972e3d91508313a4ea9320f662f2ef53b4a
Luis Scandelai <saeslhs@gmail.com>
2022-07-31 19:30:33 -0300

    Card component, closes #8

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,23 +1,33 @@
 import React, { useContext } from "react";
 import { ThemeContext } from "./context/ThemeContext";
+import { useTranslation } from "react-i18next";
 import CustomNavbar from "./components/Navbar/CustomNavbar";
 import CustomTitle from "./components/Generics/CustomTitle";
 import GenericSectionsButton from "./components/Generics/GenericSectionsButton";
 import SkillProgress from "./components/Skills/SkillProgress";
 import SVGs from "./components/Skills/SvgImports";
+import ExperienceCard from "./components/Experience/ExperienceCard";
 import "./sassStyles/_main.scss";
 
 function App() {
   const { theme } = useContext(ThemeContext);
+  const { t } = useTranslation();
 
   return (
     <div className={theme}>
       <CustomNavbar />
       <CustomTitle />
       <GenericSectionsButton />
-      {SVGs.map(({ element, text, percentage }) => (
+      {/* {SVGs.map(({ element, text, percentage }) => (
         <SkillProgress icon={element} text={text} percentage={percentage} />
-      ))}
+      ))} */}
+      <ExperienceCard
+        title={t("role0")}
+        company={t("company0")}
+        startTime={t("startTime0")}
+        endTime={t("endTime0")}
+        topics={t("topics0", { returnObjects: true })}
+      />
     </div>
   );
 }
