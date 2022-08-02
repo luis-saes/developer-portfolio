2db09e68348cef0086838d18a2ecb47eafa4d560
Luis Scandelai <saeslhs@gmail.com>
2022-08-02 13:06:23 -0300

    Created other component for horizontal tab list (mobile view), closes #9

--- front-end/src/components/Experience/ExperienceWrapper.tsx
+++ front-end/src/components/Experience/ExperienceWrapper.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
 import SVGsCompanies from "../../components/Experience/SvgImports";
+import HorizontalTabList from "./HorizontalTabList";
 import ExperienceCard from "./ExperienceCard";
 import VerticalTabList from "./VerticalTabList";
 import styles from "./ExperienceWrapper.module.scss";
@@ -8,6 +9,7 @@ import styles from "./ExperienceWrapper.module.scss";
 const ExperienceWrapper = () => {
   const { t } = useTranslation();
   const [changeCompany, setChangeCompany] = useState<number>(0);
+  const [currentActive, setCurrentActive] = useState<number>(0);
   let titles: string[] = [];
   let companies: string[] = [];
   let startTimes: string[] = [];
@@ -29,11 +31,23 @@ const ExperienceWrapper = () => {
     setChangeCompany(index);
   };
 
+  const currentActiveChange = (index: number) => {
+    setCurrentActive(index);
+  };
+
   return (
     <div className={styles.wrapper}>
+      <HorizontalTabList
+        companies={SVGsCompanies}
+        changeCompany={companyChangeHandler}
+        setCurrentActiveIndex={currentActiveChange}
+        currentActiveIndex={currentActive}
+      />
       <VerticalTabList
         companies={SVGsCompanies}
         changeCompany={companyChangeHandler}
+        setCurrentActiveIndex={currentActiveChange}
+        currentActiveIndex={currentActive}
       />
       <div style={{ marginLeft: "36px" }}>
         <ExperienceCard
