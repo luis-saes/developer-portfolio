aad6e36e10177818d765b78e0f1576504b811ef6
Luis Scandelai <saeslhs@gmail.com>
2022-08-03 11:46:10 -0300

    Footer created, closes #13

--- front-end/src/components/Footer/Footer.tsx
+++ front-end/src/components/Footer/Footer.tsx
@@ -1,35 +1,19 @@
-import React, { useContext } from "react";
-import { ThemeContext } from "../../context/ThemeContext";
+import React, { useState, useEffect } from "react";
+import useWindowDimensions from "../../utils/WindowDimensions";
+import FooterDesktop from "./FooterDesktop";
+import FooterMobile from "./FooterMobile";
 import styles from "./Footer.module.scss";
-import { ReactComponent as Logo } from "../../assets/svg/logo-dark.svg";
-import { ReactComponent as Linkedin } from "../../assets/svg/socialMedia/linkedin.svg";
-import { ReactComponent as Github } from "../../assets/svg/socialMedia/github.svg";
-import { ReactComponent as Medium } from "../../assets/svg/socialMedia/medium.svg";
 
 const Footer = () => {
-  const { theme } = useContext(ThemeContext);
+  const [expanded, setExpanded] = useState(false);
 
-  return (
-    <div className={`${styles[theme]} ${styles.wrapper}`}>
-      <Logo className={styles.logo} />
-      <div className={styles.text}>© 2022 Luis Henrique Saes Scandelai</div>
-      <div className={styles.iconsWrapper}>
-        <a
-          href="https://www.linkedin.com/in/luis-saes/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <Linkedin className={styles.icon} />
-        </a>
-        <a href="https://github.com/luis-saes" target="_blank" rel="noreferrer">
-          <Github className={styles.icon} style={{ marginLeft: "10px" }} />
-        </a>
-        <a href="https://medium.com/@luisaes" target="_blank" rel="noreferrer">
-          <Medium className={styles.icon} style={{ marginLeft: "7px" }} />
-        </a>
-      </div>
-    </div>
-  );
+  const { height, width } = useWindowDimensions();
+
+  useEffect(() => {
+    setExpanded(width > 1399);
+  }, [height, width]);
+
+  return <>{expanded ? <FooterDesktop /> : <FooterMobile />}</>;
 };
 
 export default Footer;
