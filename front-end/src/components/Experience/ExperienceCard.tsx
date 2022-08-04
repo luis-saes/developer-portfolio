8168f716a2df470abc91981e484d24ed01c29a76
Luis Scandelai <saeslhs@gmail.com>
2022-08-04 19:53:09 -0300

    Trying to solve map problem with experience cards

--- front-end/src/components/Experience/ExperienceCard.tsx
+++ front-end/src/components/Experience/ExperienceCard.tsx
@@ -8,20 +8,14 @@ type PrivateProps = {
   links: string[];
   startTime: string[];
   endTime: string[];
-  // topics: string[][];
+  topics: string[][];
   currentActive: number;
 };
 
 const ExperienceCard = (props: PrivateProps) => {
   const { theme } = useContext(ThemeContext);
 
-  // console.log(props.topics, props.currentActive);
-
-  const topics0 = [
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eu neque dignissim, mattis libero et, facilisis ipsum. In bibendum, magna ut facilisis iaculis, ipsum erat ullamcorper nibh, sed dapibus sem augue molestie neque.",
-    "Aenean vitae est tristique, finibus nunc sed, semper libero. Cras eu feugiat tortor. Nam ac ipsum at turpis pretium tristique.",
-    "Praesent pulvinar congue aliquam. Praesent non arcu tincidunt, feugiat risus vel, vehicula massa. Integer neque enim, tempor finibus erat quis, varius placerat neque. Integer sit amet ligula erat. Proin commodo non nisl nec ullamcorper.",
-  ];
+  console.log(props.topics);
 
   return (
     <div
@@ -44,7 +38,7 @@ const ExperienceCard = (props: PrivateProps) => {
         props.startTime[props.currentActive]
       } - ${props.endTime[props.currentActive]}`}</div>
       <ul>
-        {topics0.map((val, index) => (
+        {props.topics[props.currentActive].map((val, index) => (
           <li className={styles.topics} key={`${props.currentActive}${index}`}>
             {val}
           </li>
